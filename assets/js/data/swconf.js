d43b6439e9f8d7aa97111de543718f23c0acc45b
forker2 <forker2@dim0.uu.me>
2026-05-06 08:23:53 +0000

    Automatic publish from dl8z

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,1 +1,1 @@
-const swconf = { cacheName: 'chirpy-1758691631',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', '/statuspage/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
+const swconf = { cacheName: 'chirpy-1778055829',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', '/statuspage/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
